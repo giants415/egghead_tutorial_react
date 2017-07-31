fbc38444b27f60ef852029cf0c8fa040d5d0f7c6
Andrew <andrew.vinocur@gmail.com>
2017-07-31 14:48:53 -0700

    finishes video 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: 'this is the state text'
+    }
+  }
+  update(e){
+    this.setState({txt: e.target.value})
+  }
   render(){
-    let txt = this.props.txt
     return (
-      <h1>{txt}</h1>
+      <div>
+        <input type="text" onChange={this.update.bind(this)}/>
+        <h1>{this.state.txt}</h1>
+      </div>
     )
   }
 };
