041ccfc303db9755400beaacf6ea8ad158d6e20b
Andrew <andrew.vinocur@gmail.com>
2017-07-31 14:29:48 -0700

    cleans up last lesson code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-// class App extends React.Component {
-//   render(){
-//     return <h1 className="">Hello world</h1>
-//   }
-// };
+class App extends React.Component {
+  render(){
+    return <h1>Hello world</h1>
+  }
+};
 
-const App = () => <h1>Hello stateless</h1>
+// const App = () => <h1>Hello stateless</h1>
+//^^^ stateless react ///
 
 export default App
