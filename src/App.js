05094815a4486c899ee0896eaea23e2d1bffbad5
Andrew <andrew.vinocur@gmail.com>
2017-07-31 14:51:28 -0700

    finishe video 6

--- src/App.js
+++ src/App.js
@@ -13,20 +13,14 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)}/>
         <h1>{this.state.txt}</h1>
+        <Widget update={this.update.bind(this)}/>
       </div>
     )
   }
 };
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: "this is the default txt"
-}
+const Widget = (props) =>
+<input type="text" onChange={props.update} />
 
 export default App
